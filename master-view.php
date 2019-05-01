4a6764c7611e713408394468ca5caaa2138bd659
mattperkins <github@mattperkins.com>
2019-05-01 12:29:10 +0100

    restructure views path and rename

---
+++ master-view.php
@@ -0,0 +1,18 @@
+<?php
+// functions path relative to index.php (not this file)
+require_once("functions.php"); 
+ob_start();
+include_once("partials/header.php"); 
+?>
+
+<div class="container">
+
+<header><?php fileIfPartial("partials/nav.php"); ?></header>
+
+
+<?php echo $content; ?>
+
+
+</div><!-- end container -->
+
+<?php fileIfPartial("partials/footer.php"); ?>
