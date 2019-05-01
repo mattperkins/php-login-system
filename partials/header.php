f14d892d7e7ad78785db64071c2cc4b7d8988112
mattperkins <github@mattperkins.com>
2019-05-01 11:58:08 +0100

    scaffold view partial

--- partials/header.php
+++ partials/header.php
@@ -4,9 +4,6 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>PHP Login System</title>
+  <title><?php echo e($metaTitle); ?></title>
 </head>
 <body>
-<?php 
-require_once("nav.php"); 
-?>
